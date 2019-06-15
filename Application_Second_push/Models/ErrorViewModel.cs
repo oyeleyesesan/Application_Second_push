83ab432c2058a08b147c92655f0c34c565fe08e9
oyeleyesesan <oyeleyesesan@gmail.com>
2019-06-15 15:25:02 +0100

    Add project files.

---
+++ Application_Second_push/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Application_Second_push.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
